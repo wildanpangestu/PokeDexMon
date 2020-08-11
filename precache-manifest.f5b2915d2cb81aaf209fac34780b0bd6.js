3af3ab70fc936d65d9102956dc4fd1dbb54e86e7
Wildan <wildan.pangestu@outlook.com>
2020-08-11 21:05:03 +0700

    Updates

---
+++ precache-manifest.f5b2915d2cb81aaf209fac34780b0bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300fd36a6f3162a43e76f1e7146b1764",
+    "url": "/PokeDexMon/index.html"
+  },
+  {
+    "revision": "4098ab25d6beb530e5e4",
+    "url": "/PokeDexMon/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "78df73ede3a4be109472",
+    "url": "/PokeDexMon/static/css/main.faf51d99.chunk.css"
+  },
+  {
+    "revision": "4098ab25d6beb530e5e4",
+    "url": "/PokeDexMon/static/js/2.fa1e18e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PokeDexMon/static/js/2.fa1e18e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78df73ede3a4be109472",
+    "url": "/PokeDexMon/static/js/main.576a9f73.chunk.js"
+  },
+  {
+    "revision": "7b70376ab3e36a5636d2",
+    "url": "/PokeDexMon/static/js/runtime-main.da29e08b.js"
+  },
+  {
+    "revision": "4f90fdbf7780ce27511c6d631fbebabf",
+    "url": "/PokeDexMon/static/media/background.4f90fdbf.jpg"
+  },
+  {
+    "revision": "796c06223312bb8d6311e6a60c4e8f1f",
+    "url": "/PokeDexMon/static/media/spinner.796c0622.gif"
+  }
+]);
